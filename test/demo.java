ddcee5da1d3271983743d3d5d5b44d1f3bf7eb7a
mohitsharma415 <33037007+mohitsharma415@users.noreply.github.com>
2023-01-07 15:36:58 +0530

    Update demo.java

--- test/demo.java
+++ test/demo.java
@@ -2,7 +2,7 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Hi hello, World  !");
+        System.out.println("Hello Hi hello, World   !");
     }
 }
 
