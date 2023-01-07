d08398299569d95d3dff9c3c463bfdc022d3d724
mohitsharma415 <33037007+mohitsharma415@users.noreply.github.com>
2023-01-07 14:19:26 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Hi, World !");
+        System.out.println("Hello Hi hello, World !");
     }
 }
